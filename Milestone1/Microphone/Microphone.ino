556dd28abd0ce9788f236620b1c3c029c1a20230
Ian <chenghsi.yang@gmail.com>
2023-04-17 11:44:59 -0700

    adding the microphone demo

---
+++ Milestone1/Microphone/Microphone.ino
@@ -0,0 +1,52 @@
+// Modified from the example (PDMSerialPlotter) of the Seeed library
+
+#include <PDM.h>
+
+// buffer to read samples into, each sample is 16-bits
+short sampleBuffer[256];
+
+// number of samples read
+volatile int samplesRead;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) yield();
+
+  // configure the data receive callback
+  PDM.onReceive(onPDMdata);
+
+  // optionally set the gain, defaults to 20
+  // PDM.setGain(30);
+
+  // initialize PDM with:
+  // - one channel (mono mode)
+  // - a 16 kHz sample rate
+  if (!PDM.begin(1, 16000)) {
+    Serial.println("PDM Failed to Initialize!");
+    while (1) yield();
+  }
+}
+
+void loop() {
+  // wait for samples to be read
+  if (samplesRead) {
+    // print samples to the serial monitor or plotter
+    for (int i = 0; i < samplesRead; i++) {
+      Serial.println(sampleBuffer[i]);
+    }
+    // clear the read count
+    samplesRead = 0;
+  }
+}
+
+void onPDMdata() {
+  // query the number of bytes available
+  int bytesAvailable = PDM.available();
+
+  // read into the sample buffer
+  PDM.read(sampleBuffer, bytesAvailable);
+
+  // 16-bit, 2 bytes per sample
+  samplesRead = bytesAvailable / 2;
+}
+
