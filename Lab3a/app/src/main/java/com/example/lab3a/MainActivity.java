4cc0168974efcc96332de17b468510a940947ffd
Ian <chenghsi.yang@gmail.com>
2023-04-28 23:12:17 -0700

    uploading the android project for lab three part a

---
+++ Lab3a/app/src/main/java/com/example/lab3a/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.lab3a;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                ImageView image = (ImageView)findViewById(R.id.image);
+                image.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+}
