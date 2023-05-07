c1101f48322d6e0646bc3853f188929b35a0c650
Ian <chenghsi.yang@gmail.com>
2023-05-07 00:37:42 -0700

    implementing colors for the LED

--- Milestone2/Indicators/Indicators.ino
+++ Milestone2/Indicators/Indicators.ino
@@ -1,30 +1,36 @@
-// Modified from the Arduino NeoPixel example
-
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
-#define PIN 10
-#define NUMPIXELS 1
+#define PIN_NEO_PIXEL  0
+#define NUM_PIXELS     1
 
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-#define DELAYVAL 500
+Adafruit_NeoPixel NeoPixel(NUM_PIXELS, PIN_NEO_PIXEL, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  Serial.begin(9600);
+  NeoPixel.begin();
 }
 
 void loop() {
-  pixels.clear();
-
-  for(int i=0; i<NUMPIXELS; i++) {
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-    pixels.show();
-    delay(DELAYVAL);
-  }
+  // White: 255, 255, 255
+  NeoPixel.setPixelColor(0, NeoPixel.Color(255, 255, 255)); // WHITE
+  NeoPixel.show();
+  delay(2000);
+  NeoPixel.setPixelColor(0, NeoPixel.Color(0, 0, 255)); // BLUE
+  NeoPixel.show();
+  delay(2000);
+  NeoPixel.setPixelColor(0, NeoPixel.Color(255, 255, 50)); // YELLOW
+  NeoPixel.show();
+  delay(2000);
+  NeoPixel.setPixelColor(0, NeoPixel.Color(255, 0, 0)); // RED
+  NeoPixel.show();
+  delay(2000);
+  NeoPixel.setPixelColor(0, NeoPixel.Color(0, 255, 0)); // GREEN
+  NeoPixel.show();
+  delay(2000);
+  NeoPixel.setPixelColor(0, NeoPixel.Color(255, 0, 255)); // PURPLE
+  NeoPixel.show();
+  delay(2000);
 }
