d7365d8c311d4d49cc0d10ca3e8e963a8f7bf760
Ian <chenghsi.yang@gmail.com>
2023-05-31 23:53:58 -0700

    Update README.md

--- Milestone3/Level_Converter/Level_Converter.ino
+++ Milestone3/Level_Converter/Level_Converter.ino
@@ -1,12 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(10, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(10, HIGH);
+  digitalWrite(5, HIGH);
   delay(1000);
-  digitalWrite(10, LOW);
+  digitalWrite(5, LOW);
   delay(1000);
 }
